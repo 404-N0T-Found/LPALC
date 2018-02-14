65080f6df15282ebcb7a8bf8649ba07913267f76
Meysam Azad <MeysamAzad81@yahoo.com>
2018-02-14 18:16:20 +0330

    add nodes, edges & numberOfNodes to Network.h

--- src/network.h
+++ src/network.h
@@ -4,10 +4,19 @@
 #include "node.h"
 class Node; // forward declaration
 
+#include "../Sparse-Matrix/src/SparseMatrix/SparseMatrix.cpp"
+
 class Network
 {
+private:
+    int numberOfNodes;
+    Node* nodes;
+
+    SparseMatrix<int> *edges;
 public:
     Network();
+
+    bool init(string inputPath);
 };
 
 #endif // NETWORK_H
