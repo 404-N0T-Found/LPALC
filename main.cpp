c94f7d61548d1a5651b678240999ab9535d4d09a
Meysam Azad <MeysamAzad81@yahoo.com>
2018-02-15 12:26:39 +0330

    main.cpp (under construction)

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
-#include <QCoreApplication>
+#include <src/network.h>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    Network network;
 
-    return a.exec();
+    string outputPath = "../test\ case/output.txt";
+
+
+    if (argc == 2) // just input file passed as command line argument
+    {
+        network.init(argv[1]);
+    }
+    else if (argc == 3) // input and output
+    {
+        network.init(argv[1]);
+        outputPath = argv[2];
+    }
+    else // none
+    {
+        cout << "Usage: main.o <input file path>"
+                " [<output file path>]\n";
+        return 4;
+    }
+
+    if (network.mainFunc());
+
+    return 0;
 }
-
