51dc141a699017d45d3e834f5ea426de67943321
Meysam Azad <MeysamAzad81@yahoo.com>
2018-02-15 12:25:53 +0330

    vectorizing nodes, adding mainFunc & NMI

--- src/network.cpp
+++ src/network.cpp
@@ -2,7 +2,6 @@
 
 Network::Network() : numberOfNodes(0)
 {
-
 }
 
 bool Network::init(string inputPath)
@@ -43,10 +42,13 @@ bool Network::init(string inputPath)
         // node labels start from zero so we should initializr from 1 to n
         edges = new SparseMatrix<int>(numberOfNodes);
 
-        nodes = new Node[numberOfNodes + 1];
+        nodes = new vector<Node>(numberOfNodes + 1);
         for (int i = 1; i <= numberOfNodes; ++i)
-            nodes[i].label = i;
-
+        {
+            nodes->at(i).label = i;
+            nodes->at(i).degree = 0;
+            nodes->at(i).newLabel = i;
+        }
 
 
         // second loop to get the edges
@@ -69,8 +71,8 @@ bool Network::init(string inputPath)
             edges->set(v2, v1, 1);
 
             // add the corresponding degree
-            nodes[v1].degree++;
-            nodes[v2].degree++;
+            nodes->at(v1).degree++;
+            nodes->at(v2).degree++;
 
         }
 
@@ -88,3 +90,79 @@ bool Network::init(string inputPath)
     }
 }
 
+bool Network::mainFunc()
+{
+    int t = 1;
+    while (NMI() >= exp(1)
+           && (t <= numberOfNodes))
+    {
+        unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+        std::shuffle(nodes->begin() + 1, nodes->end(), default_random_engine(seed));
+
+
+        for (int i = 1; i <= numberOfNodes; ++i)
+        {
+            vector<int> neighbors = edges->getNeighbors(i);
+
+
+
+            /* the calculations are based on the labels or as we call it 'newLabel'
+             * not the node number itself which is just a distinction of nodes
+             */
+            for (int i = 0; i < neighbors.size(); i++)
+            {
+                int tmp = neighbors.at(i);
+                neighbors.at(i) = nodes->at(tmp).newLabel;
+            }
+
+
+
+            map<int, int> neighborFreq;
+            foreach (int j, neighbors)
+                if (neighborFreq.count(j) == 0)
+                    neighborFreq.insert(make_pair(j, count(neighbors.begin(),
+                                                           neighbors.end(),
+                                                           j)));
+
+
+            map<int, int>::iterator iter = neighborFreq.begin();
+
+            int numberOfMax = 1;
+            int maxIndex = iter->first,
+                    maxFreq = iter->second;
+            iter++;
+
+            for (; iter != neighborFreq.end(); iter++)
+            {
+                if (iter->second > maxFreq)
+                {
+                    numberOfMax = 1;
+                    maxIndex = iter->first;
+                    maxFreq = iter->second;
+                }
+                else if (iter->second == maxFreq)
+                    numberOfMax++;
+            }
+
+            /* difference between LPA and LPACL happens here cause it looks
+             * for cycles in case of multiple labels having the same number
+             * of repetition in a node's neighbors list
+             */
+            if (numberOfMax > 1)
+            {
+
+            }
+            else // only one type of labels are in the node's neighbors list
+            {
+
+            }
+
+        }
+        t++;
+    }
+}
+
+int Network::NMI()
+{
+    return 10;
+}
