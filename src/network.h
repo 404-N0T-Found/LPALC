d99dc3a91ec9ac5cd800d4a2e881433cfa95944f
Meysam Azad <MeysamAzad81@yahoo.com>
2018-02-15 12:24:34 +0330

    add required libraries, struct Node, and required class members of class Network

--- src/network.h
+++ src/network.h
@@ -1,22 +1,48 @@
 #ifndef NETWORK_H
 #define NETWORK_H
 
-#include "node.h"
-class Node; // forward declaration
-
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <QFile>
+#include <iomanip>
+#include <QTime>
+#include <fstream>
+#include <math.h>
+#include <random>
+#include <chrono>
+#include <vector>
+#include <algorithm>
+#include <map>
+// reference: https://github.com/meysam81/Sparse-Matrix
 #include "../Sparse-Matrix/src/SparseMatrix/SparseMatrix.cpp"
+
+using namespace std;
+
+typedef struct Node
+{
+    int label;
+    int newLabel;
+    int degree;
+} Node;
 
 class Network
 {
 private:
     int numberOfNodes;
-    Node* nodes;
+    vector<Node> *nodes;
 
     SparseMatrix<int> *edges;
+
+    int numberOfRealNetwork; // cA
+
+    int NMI();
 public:
     Network();
 
     bool init(string inputPath);
+
+    bool mainFunc();
 };
 
 #endif // NETWORK_H
